c0a356c51d19cea604da6b8b5edddf58fd41e160
Daniel Waghorn <daniel@daniel-waghorn.co.uk>
2018-03-05 20:38:19 +0000

    Improve UserModel

--- models/user.js
+++ models/user.js
@@ -10,6 +10,7 @@
  */
 
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt');
 
@@ -18,29 +19,32 @@ SALT_WF = 8;
 var UserSchema = new Schema({
 	forename: { type: String, required: true },
 	surname: { type: String, required: true },
-	email: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	admin: { type: Boolean }
 });
 
-UserSchema.pre('save', function(next) {
-    UserModel.findOne({ email: this.email }, function(err, userExists) {
-        if (userExists) {
-            return next(new Error('A user already exists for this email address!'));
-        } else {
-            if (!this.isModified('password')) return next();
+UserSchema.plugin(uniqueValidator);
 
-            bcrypt.genSalt(SALT_WF, function(err, salt) {
-                if (err) return next(err);
-        
-                bcrypt.hash(this.password, salt, function(err, hash) {
-                    if (err) return next(err);
-                    this.password = hash;
-                    next();
-                });
-            });
-        }
-    });
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  // Only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WF, function(err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
 });
 
 if (!UserSchema.options.toJSON) UserSchema.options.toJSON = {};
