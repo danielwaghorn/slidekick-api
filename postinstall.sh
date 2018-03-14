6b23043c5b9f3574f39c010c214be61b83e051d7
Daniel Waghorn <daniel@daniel-waghorn.co.uk>
2018-03-14 17:47:14 +0000

    Disable postinstall.sh on production

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-export MONGODB_PASS=password
-echo "MongoDB Password is '$MONGODB_PASS'"
-docker pull tutum/mongodb
+if [ "$HOSTNAME" != dw-vps-1 ]; then
+  export MONGODB_PASS=password
+  echo "MongoDB Password is '$MONGODB_PASS'"
+  docker pull tutum/mongodb
+fi
