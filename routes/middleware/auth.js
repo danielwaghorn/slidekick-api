6c2902cb4236db3113978225e504fc0d7f9a034e
Daniel Waghorn <daniel@daniel-waghorn.co.uk>
2018-03-15 15:21:03 +0000

    Refactor auth middleware

---
+++ routes/middleware/auth.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken') // used to create, sign, and verify tokens
+const User = require('../../models/user.js')
+
+/**
+ * Token Middleware
+ *
+ * Validates a JWT token presented on Authorization
+ * header.
+ *
+ * @param  {Object} req   Express request object
+ * @param  {Object} res   Express response object
+ * @param  {Function} next Closure for next request
+ * @return {void}
+ */
+
+const jwtMiddleware = function (req, res, next) {
+    // Ignore token requirement for registration and login
+  if (['/register', '/login'].indexOf(req.path) !== -1) return next()
+
+    var token = req.body.token || req.params.token || req.headers['authorization']
+    if (token === undefined) token = ''
+    token = token.replace('Bearer ', '')
+
+    // Verify that token exists and is valid;
+    if (token) {
+      jwt.verify(token, req.app.get('secret'), function (err, decoded) {
+          if (err) return res.status(401).json({ success: false, message: err.message })
+            User.findById(decoded.id, (err, user) => {
+              if (user) {
+                  req.user = user
+                    return next()
+                }
+
+              res.status(401).send({
+                  success: false,
+                  message: 'Bad Token'
+                })
+            })
+        })
+    } else {
+      return res.status(401).send({
+          success: false,
+          message: 'Bad Token'
+        })
+    }
+}
+
+module.exports = jwtMiddleware
